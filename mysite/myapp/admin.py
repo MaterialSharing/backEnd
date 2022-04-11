817bc1cc2953b1a93aa6736b13ad1adea1ec37ed
cxxu <838808930@qq.com>
2022-04-11 18:14:43 +0800

    jetBrain configs

--- mysite/myapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from myapp.models import myapp
-
-admin.site.register(user)
--- mysite/myapp/scripts.py
+++
@@ -1,6 +0,0 @@
-from myapp.models import User
-
-mod=User.objects
-l=mod.all()
-for item in l:
-    print(item)

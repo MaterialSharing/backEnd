fa8340859993083be64c5259c987af6179b8408f
cxxu <838808930@qq.com>
2022-06-08 21:28:42 +0800

    fix the webhook auto deployment bug

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
+# please edit (modify) the file in the dev environment only (or write the deploy.sh and commit & push the branch at once!otherwise,the modification would be lost!
 cd ~/backEnd/
 #git status
-git pull origin/main
-git status
+git reset --hard origin/main
+git log |tail -n 10
+#git pull origin/main
+git status|head -n 10
+echo '----brute force pull done!(by reset to the remote origin/main)'

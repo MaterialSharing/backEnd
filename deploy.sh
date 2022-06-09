7c6a68b235a306e649bd3a9f0e1abe3d8882fbe0
cxxu <838808930@qq.com>
2022-06-09 15:00:58 +0800

    fix webhook

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 # please edit (modify) the file in the dev environment only (or write the deploy.sh and commit & push the branch at once!otherwise,the modification would be lost!
 # for the initial webhook to work,please manually run`git pull origin main_or_otherBranch`,to get the deploy.sh available in the remote end
-cd ~/backEnd/
+# 使用某些路径符号`~`会导致报错,比如:cd ~/backEnd/
+cd /home/cxxu/backEnd
 #git status
 git reset --hard origin/main
 git log |tail -n 10
